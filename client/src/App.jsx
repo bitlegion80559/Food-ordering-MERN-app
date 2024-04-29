9d3a421d43eadbaedbedb204120becd91d2fdc1a
Umesh <umeshnagar9152@gmail.com>
2024-04-30 00:01:25 +0530

    pages rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
 import React,{lazy} from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import ProtectRoute from './components/auth/ProtectRoute';
 const Home = lazy(() => import("./pages/Home"));
 const About=lazy(()=>import("./pages/About"));
 const Login=lazy(()=>import("./pages/Login"));
 const Groups=lazy(()=>import("./pages/Groups"));
 const Chat=lazy(()=>import("./pages/Chat"));
+const NotFound=lazy(()=>import("./pages/NotFound"));
+let user=false;
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+      <Route
+            element={
+             <ProtectRoute user={user} />
+            }
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
         <Route path='/about' element={<About/>}/>
-        <Route path='/groups' element={<Groups/>}/>
-        <Route path='/chat/:chatId' element={<Chat/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+          <Route path='*' element={<NotFound/>}/>
       </Routes>
     </Router>
   )
